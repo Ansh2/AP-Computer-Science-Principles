3cd6d422046adb8300d4059c41d5c4e33e24071f
Ansh2 <ansh.nynj@gmail.com>
2022-12-08 17:31:47 -0500

    first commit

---
+++ TicTacToeGame.java
@@ -0,0 +1,16 @@
+public class TicTacToeGame {
+	public static final char CROSS = 'X';
+	public static final char CIRCLE = 'O';
+	private char[][] board; 
+	
+
+	public TicTacToeGame() {
+		board = new char[3][3]; 
+		System.out.println("Do you want to go first?");
+		
+		
+	}
+	
+	
+
+}
